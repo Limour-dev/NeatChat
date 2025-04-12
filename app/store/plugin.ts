879ecf9beacd71d532f887927df8fc05796ae9c4
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-04-12 19:28:00 +0800

    fix: 预设插件可修改
    https://github.com/tianzhentech/NeatChat/issues/37

--- app/store/plugin.ts
+++ app/store/plugin.ts
@@ -240,18 +240,20 @@ export const usePluginStore = createPersistStore(
         .then((res) => res.json())
         .then((res) => {
           Promise.all(
-            res.map((item: any) =>
-              // skip get schema
-              state.get(item.id)
-                ? item
-                : fetch(item.schema)
-                    .then((res) => res.text())
-                    .then((content) => ({
-                      ...item,
-                      content,
-                    }))
-                    .catch((e) => item),
-            ),
+            res
+              .filter((item: any) => !state.get(item.id))
+              .map((item: any) =>
+                // skip get schema
+                state.get(item.id)
+                  ? item
+                  : fetch(item.schema)
+                      .then((res) => res.text())
+                      .then((content) => ({
+                        ...item,
+                        content,
+                      }))
+                      .catch((e) => item),
+              ),
           ).then((builtinPlugins: any) => {
             builtinPlugins
               .filter((item: any) => item?.content)
@@ -261,7 +263,7 @@ export const usePluginStore = createPersistStore(
                   const tool = FunctionToolService.add(plugin, true);
                   plugin.title = tool.api.definition.info.title;
                   plugin.version = tool.api.definition.info.version;
-                  plugin.builtin = true;
+                  plugin.builtin = false;
                 });
               });
           });
